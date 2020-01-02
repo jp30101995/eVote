ce98f9834b240ad4059dcb9e3039f79688122282
Jaimin-Patel30 <jaimin.patel@oneadvanced.com>
2020-01-02 18:29:08 +0530

    adding api

---
+++ .history/api/validatorapi/handler_20200102180816.js
@@ -0,0 +1,37 @@
+'use strict';
+const AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.AWS_REGION })
+
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' })
+
+module.exports.hello = async event => {
+  console.log(event);
+  const aadhar = event.queryStringParameters.aadhar;
+
+  var params = {
+    TableName: 'masterdata',
+    ExpressionAttributeNames: {
+      '#aadharNo': 'aadhar_no'
+    },
+    ExpressionAttributeValues: {
+      ':aadharNo': event.data
+    },
+    FilterExpression: '#aadharNo = :aadharNo'
+  };
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
